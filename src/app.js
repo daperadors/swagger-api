16ca4e7ab202d0350807c7bfa6e760aed6ae7ab7
david <david@adderit.es>
2023-02-07 13:08:31 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const requestsRoute = require("./routes/requests");
+const path = require("path");
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Swagger API",
+      version: "1.0.0"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ]
+  },
+  apis: [`${path.join(__dirname, "./routes/*.js")}`]
+}
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+app.use("/api", requestsRoute);
+app.use("/api/documentation", swaggerUI.serve, swaggerUI.setup(swaggerJSDoc(swaggerSpec)));
+
+app.get("/", (req, res) => {
+  res.send("Swagger API");
+});
+
+app.listen(port, () => console.log("Server listening to", port));
---
+++ src/routes/requests.js
@@ -0,0 +1,151 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Users:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: id of user
+ *        name:
+ *          type: string
+ *          description: name of user
+ *      required:
+ *        - name
+ *      example:
+ *        - name: david
+ */
+
+/**
+ * @swagger
+ * /api/get:
+ *  get:
+ *    summary: return all users
+ *    tags: [Users]
+ *    responses:
+ *      200:
+ *        description: users
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Users'
+*/
+router.get("/get", (req, res)=>{
+  const data = [
+    {id: 1, name: "david"},
+    {id: 2, name: "oriol"},
+    {id: 3, name: "jordi"}
+  ]
+  res.json(data);
+});
+/**
+ * @swagger
+ * /api/get/{id}:
+ *  get:
+ *    summary: return a user
+ *    tags: [Users]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: User id.
+ *    responses:
+ *      200:
+ *        description: a user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/Users'
+ *      404:
+ *        description: user not found.
+*/
+router.get("/get/:id", (req, res)=>{
+  let {id} = req.params;
+  const data = {id: id, name: "david"}
+  res.json(data);
+});
+
+/**
+ * @swagger
+ * /api/post:
+ *  post:
+ *    summary: create a new user
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/Users'
+ *    responses:
+ *      200:
+ *        description: User created.
+*/
+router.post("/post", (req, res)=>{
+  res.json({created: true});
+});
+/**
+ * @swagger
+ * /api/put/{id}:
+ *  put:
+ *    summary: update a user
+ *    tags: [Users]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: User id.
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/Users'
+ *    responses:
+ *      200:
+ *        description: user updated
+ *      404:
+ *        description: user not found.
+*/
+router.put("/put/:id", (req, res)=>{
+  let {id} = req.params;
+  res.json({updated: true});
+});
+/**
+ * @swagger
+ * /api/delete/{id}:
+ *  delete:
+ *    summary: delete a user
+ *    tags: [Users]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: User id.
+ *    responses:
+ *      200:
+ *        description: user deleted
+ *      404:
+ *        description: user not found.
+*/
+router.delete("/delete/:id", (req, res)=>{
+  let {id} = req.params;
+  res.json({deleted: true});
+});
+
+module.exports = router;
